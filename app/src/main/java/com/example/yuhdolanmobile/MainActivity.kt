dc194991dc4a7ce62e17e3981f85d39d80c11d7a
hilmyha <hilmyahmadhaidar@gmail.com>
2023-07-05 22:45:12 +0700

    update | make UI/UX LoginActivity.kt, Fragment

--- app/src/main/java/com/example/yuhdolanmobile/MainActivity.kt
+++ app/src/main/java/com/example/yuhdolanmobile/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.yuhdolanmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.yuhdolanmobile.Fragment.FavoriteFragment
+import com.example.yuhdolanmobile.Fragment.HomeFragment
+import com.example.yuhdolanmobile.Fragment.ProfileFragment
+import com.example.yuhdolanmobile.Fragment.UlasanFragment
+import com.example.yuhdolanmobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.navbar.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_favorite -> replaceFragment(FavoriteFragment())
+                R.id.nav_ulasan -> replaceFragment(UlasanFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_navbar, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/yuhdolanmobile/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.yuhdolanmobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val goto = Intent(this, MainActivity::class.java)
+            startActivity(goto)
+            finish()
+        }, 3000)
+
+    }
+}
